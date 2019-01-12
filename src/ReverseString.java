cc3e2a29d78fecf2843df172bc6a939af35cb43e
shashank gupta <shashank_32@live.com>
2019-01-11 21:00:54 -0500

    Update src/ReverseString.java
    Reverses string input

---
+++ src/ReverseString.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+
+public class ReverseString {
+
+
+	
+	private static Scanner read;
+
+	public static void main(String[] args) {
+
+		Boolean toggle = true;
+		
+		while(toggle == true) {
+			System.out.println("Enter string you would like to reverse");
+			
+			read = new Scanner(System.in);
+			String word = read.nextLine();
+			stringReverse(word);
+			
+			System.out.println("Continue? y or n");
+			word = read.nextLine();
+			
+			if(word.contains("y")) {
+				toggle = true;
+			}else if (word.contains("n")) {
+				toggle = false;
+			}
+		}
+		
+	}
+	
+	public static void stringReverse(String stringToReverse) {
+		
+		
+		String emptyString = "";
+		
+		for(int i=stringToReverse.length()-1; i>=0; i--) {
+			emptyString = emptyString + stringToReverse.charAt(i);
+		}
+		System.out.println("Result:");
+		System.out.println(emptyString);
+		
+	}
+}
